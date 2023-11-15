b62890e976ca3003788812f3044da613bda5b1a5
Linus Vettiger <lv@vlot.ch>
2023-11-15 22:16:21 +0100

    cleanup-wip

---
+++ lib/System/Hardware/Devices/StreamDeckMk2.hs
@@ -0,0 +1,20 @@
+module System.Hardware.Devices.StreamDeckMk2 where
+
+import Internal.Prelude
+import System.HIDAPI qualified as HID
+import System.Hardware.StreamDeck
+
+newtype StreamDeckMk2 = StreamDeckMk2
+  { deviceInfo :: HID.DeviceInfo
+  }
+  deriving stock (Generic)
+
+instance IsDevice StreamDeckMk2 where
+    fromDeviceInfo = StreamDeckMk2
+
+instance IsStreamDeck StreamDeckMk2 where
+    deviceIdentifier = 0x0080
+    buttonRows = 3
+    buttonCols = 5
+    
+instance IsStreamDeckWithDisplayButtons StreamDeckMk2
