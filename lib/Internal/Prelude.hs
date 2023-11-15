47e41878c1f9fbc9abe68e0980278f6d9a13c1f8
Linus Vettiger <lv@vlot.ch>
2023-11-15 22:16:21 +0100

    cleanup-wip

--- lib/Internal/Prelude.hs
+++ lib/Internal/Prelude.hs
@@ -1,18 +1,26 @@
 module Internal.Prelude
-    ( module Control.Monad
-    , module Data.Bits
-    , module Data.ByteString
-    , module Data.Functor
-    , module Data.Ord
-    , module Data.Word
-    , module Prelude
-    )
+  ( module Control.Monad,
+    module Control.Monad.Reader,
+    module Data.Bits,
+    module Data.ByteString,
+    module Data.Functor,
+    module Control.Lens.Combinators,
+    module Control.Lens.Operators,
+    module Data.Ord,
+    module Data.Word,
+    module GHC.Generics,
+    module Prelude,
+  )
 where
 
+import Control.Lens.Combinators (view)
+import Control.Lens.Operators
 import Control.Monad
+import Control.Monad.Reader
 import Data.Bits
 import Data.ByteString (ByteString)
 import Data.Functor
 import Data.Ord
 import Data.Word (Word16, Word8)
+import GHC.Generics
 import Prelude
