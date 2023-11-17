09b10aa7bf93e5a1e2bc69affd0d6c8feaf66cef
Linus Vettiger <lv@vlot.ch>
2023-11-18 00:46:25 +0100

    enrich isdevice

--- lib/Internal/Prelude.hs
+++ lib/Internal/Prelude.hs
@@ -1,8 +1,11 @@
 module Internal.Prelude
     ( module Control.Monad
+    , module Control.Monad.Base
+    , module Control.Monad.Fix
     , module Control.Monad.Reader
     , module Data.Bits
     , module Data.ByteString
+    , module Data.Maybe
     , module Data.Functor
     , module Control.Lens.Combinators
     , module Control.Lens.Operators
@@ -19,11 +22,14 @@ where
 import Control.Lens.Combinators (view)
 import Control.Lens.Operators
 import Control.Monad
+import Control.Monad.Base (MonadBase (liftBase))
+import Control.Monad.Fix
 import Control.Monad.Reader
 import Data.Bits
 import Data.ByteString (ByteString)
 import Data.Functor
 import Data.Generics.Labels ()
+import Data.Maybe
 import Data.Ord
 import Data.Word (Word16, Word8)
 import GHC.Generics
--- lib/System/Hardware/Devices/StreamDeckMk2.hs
+++ lib/System/Hardware/Devices/StreamDeckMk2.hs
@@ -1,19 +1,13 @@
 module System.Hardware.Devices.StreamDeckMk2 where
 
-import Internal.Prelude
-import System.HIDAPI qualified as HID
 import System.Hardware.StreamDeck
 
-newtype StreamDeckMk2 = StreamDeckMk2
-    { deviceInfo :: HID.DeviceInfo
-    }
-    deriving stock (Generic)
+data StreamDeckMk2
 
 instance IsDevice StreamDeckMk2 where
-    fromDeviceInfo = StreamDeckMk2
+    deviceIdentifier = 0x0080
 
 instance IsStreamDeck StreamDeckMk2 where
-    deviceIdentifier = 0x0080
     buttonRows = 3
     buttonCols = 5
 
